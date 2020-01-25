b993ebd443e6268fd5487f056b71b0e8014f6505
colelarsen <larsenca@appstate.edu>
2020-01-25 12:09:40 -0800

    attachment

--- MiscCommands.js
+++ MiscCommands.js
@@ -88,12 +88,13 @@ function spoiler(mesg)
 function dude(mesg)
 {
     mesg.channel.send("HAHA DUDE WEED");
-    mesg.channel.send({
-        files: ['https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D762212417281562&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fpages%2Fcategory%2FApp-Page%2FWeedmoji-750280318474772%2F&docid=MYaGp5qZ3wXzvM&tbnid=CIcP7_aNt-FHBM%3A&vet=10ahUKEwjh1K6Knp_nAhWRnOAKHZeaCEIQMwg7KAAwAA..i&w=640&h=640&bih=967&biw=1920&q=weedmoji&ved=0ahUKEwjh1K6Knp_nAhWRnOAKHZeaCEIQMwg7KAAwAA&iact=mrc&uact=8']
-    });
-    mesg.channel.send({
-        files: ['https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D762212417281562&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fpages%2Fcategory%2FApp-Page%2FWeedmoji-750280318474772%2F&docid=MYaGp5qZ3wXzvM&tbnid=CIcP7_aNt-FHBM%3A&vet=10ahUKEwjh1K6Knp_nAhWRnOAKHZeaCEIQMwg7KAAwAA..i&w=640&h=640&bih=967&biw=1920&q=weedmoji&ved=0ahUKEwjh1K6Knp_nAhWRnOAKHZeaCEIQMwg7KAAwAA&iact=mrc&uact=8']
-    });
+    channel.send({
+        files: [{
+          attachment: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D762212417281562&imgrefurl=https%3A%2F%2Fwww.facebook.com%2Fpages%2Fcategory%2FApp-Page%2FWeedmoji-750280318474772%2F&docid=MYaGp5qZ3wXzvM&tbnid=CIcP7_aNt-FHBM%3A&vet=10ahUKEwjh1K6Knp_nAhWRnOAKHZeaCEIQMwg7KAAwAA..i&w=640&h=640&bih=967&biw=1920&q=weedmoji&ved=0ahUKEwjh1K6Knp_nAhWRnOAKHZeaCEIQMwg7KAAwAA&iact=mrc&uact=8',
+          name: 'DUDEWEED.jpg'
+        }]
+      })
+
     mesg.channel.send("HAHA DUDE");
 }
 
